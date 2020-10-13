4d9f938ec4d4607dcdc8220ef000e3e0ed654506
Amaury <19170087@uttcampus.edu.mx>
2020-10-13 01:39:12 -0500

    modifique ids, agregue textview para resultados, cree variables para cada boton

--- app/src/main/java/com/amauryrdz1/calculadora/MainActivity.java
+++ app/src/main/java/com/amauryrdz1/calculadora/MainActivity.java
@@ -13,6 +13,26 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button cero;
+    private Button uno;
+    private Button dos;
+    private Button tres;
+    private Button cuatro;
+    private Button cinco;
+    private Button seis;
+    private Button siete;
+    private Button ocho;
+    private Button nuevo;
+    private Button suma;
+    private Button resta;
+    private Button divi;
+    private Button multi;
+    private Button equ;
+    private Button del;
+    private Button pc;
+    private Button deci;
+    private Button mm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,21 +40,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void miclick(View v){
-        switch (v.getId()){
-            case R.id.btn1:
-                TextView tt= (TextView) findViewById(R.id.lbl1);
-                tt.setText("1");
-                break;
-            case R.id.btn2:
-                Toast.makeText(this, "holaaa", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.lbl1:
-                TextView lbl = (TextView) view;
-                Toast.makeText(this, "holaaa", Toast.LENGTH_LONG).show();
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + v.getId());
-        }
+
+    private void setupUi(){
+        uno = (Button)findViewById(R.id.btn1);
+        dos = (Button)findViewById(R.id.btn2);
+        tres = (Button)findViewById(R.id.btn3);
+        cuatro = (Button)findViewById(R.id.btn4);
+        cinco = (Button)findViewById(R.id.btn5);
+        seis = (Button)findViewById(R.id.btn6);
+        siete = (Button)findViewById(R.id.btn7);
+        ocho = (Button)findViewById(R.id.btn8);
+        nuevo = (Button)findViewById(R.id.btn9);
+        cero = (Button)findViewById(R.id.btn0);
+        suma = (Button)findViewById(R.id.mas);
+        resta = (Button)findViewById(R.id.menos);
+        divi = (Button)findViewById(R.id.div);
+        pc = (Button)findViewById(R.id.percent);
+        deci = (Button)findViewById(R.id.dec);
+        del = (Button)findViewById(R.id.borrar);
+        equ = (Button)findViewById(R.id.igual);
+        multi = (Button)findViewById(R.id.por);
+        mm = (Button)findViewById(R.id.masmen);
     }
 }
